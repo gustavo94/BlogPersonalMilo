c44a333190940c8d0b5163ab23f4dded1da9b96f
juan Osorio <juancaoji@gmail.com>
2016-05-08 22:52:25 -0500

    Creacion de la app
    Se corrio el comando rails new
    se modifico el .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogPersonalMilo::Application.config.secret_token = '5c6f52668bc961df99d0ace864d5f7f73dade458771cc740277aee2f19700e4df9cfd985761d1b31374658353b70a3417497a6292492b34ce2defa850d5b9ede'
